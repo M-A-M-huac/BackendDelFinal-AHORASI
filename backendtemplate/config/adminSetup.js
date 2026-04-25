d2eea4adc57f5a5d539c4fc9786d8fb9114ee568
Matias <angelicomatias@gmail.com>
2026-04-24 18:20:04 -0600

    RENDER FUNCIONA LOCO PORFAVORRRRRRRRRRRRRRRRR

--- backendtemplate/config/adminSetup.js
+++ backendtemplate/config/adminSetup.js
@@ -6,10 +6,7 @@ dotenv.config();
 
 const createAdminUser = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     const adminExists = await User.findOne({ email: 'MAGICS' });
 
